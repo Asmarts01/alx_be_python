1e8f14561f0478ab1bdba8fbe360f8ccd2665f19
Asmarts01 <aigbogunanointed71@gmail.com>
2025-07-23 12:06:39 +0100

    adjusted

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,1 +1,10 @@
 # pattern drawing in form of squares
+size = int(input("Enter the size of the pattern: "))
+row = 0
+while row < size:
+    col = 0
+    while col < size:
+        print("*", end=" ")
+        col += 1
+    print()  # Move to the next line after finishing a row
+    row += 1
