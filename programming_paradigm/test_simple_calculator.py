671ea58718f8a91c045c3ebb208dafc3ad2b3260
Asmarts01 <aigbogunanointed71@gmail.com>
2025-07-31 21:02:56 +0100

    test: add unit tests for SimpleCalculator operations

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,21 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setup(self):
+        self.calc = SimpleCalculator()
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(4, 2), 2)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 7), 21)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(5, 0), None)
+    
+    if __name__ == '__main__':
+        unittest.main()
